0e06d8bb6887adf140e0b9a4d7c249ac9338e096
Jonathan Davidson <jdavidson@netsplasher.com>
2025-09-16 17:01:50 -0500

    Removed preact contact form

--- js/contact-form.jsx
+++
@@ -1,7 +0,0 @@
-import { h, render } from 'preact';
-import ContactForm from './components/ContactForm';
-
-const element = document.getElementById('hugo-app-preact-element__contact-form');
-if (element) {
-  render(<ContactForm />, element);
-}
